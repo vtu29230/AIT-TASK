950058458c3a270275926ad08a7fbd754cd744e7
vtu29230 <vtu29230@veltech.edu.in>
2026-08-25 14:34:37 +0530

    Add files via upload

---
+++ ait task 6.py
@@ -0,0 +1,55 @@
+class Graph:
+    def __init__(self, vertices):
+        self.v = vertices
+        self.graph = [[0 for column in range(vertices)]
+                       for row in range(vertices)]
+
+    # Check if the current color assignment is safe
+    def is_safe(self, v, color, c):
+        for i in range(self.v):
+            if self.graph[v][i] == 1 and color[i] == c:
+                return False
+        return True
+
+    # Recursive function to solve m-coloring problem
+    def graph_color_util(self, m, color, v):
+        if v == self.v:
+            return True
+
+        for c in range(1, m + 1):
+            if self.is_safe(v, color, c):
+                color[v] = c
+
+                if self.graph_color_util(m, color, v + 1):
+                    return True
+
+                # Backtracking
+                color[v] = 0
+
+        return False
+
+    def graph_coloring(self, m):
+        color = [0] * self.v
+
+        if not self.graph_color_util(m, color, 0):
+            return False
+
+        print("Solution exists and following are the assigned colors:")
+        for c in color:
+            print(c, end=" ")
+
+
+# Driver Code
+if __name__ == '__main__':
+    g = Graph(4)
+
+    g.graph = [
+        [0, 1, 1, 1],
+        [1, 0, 1, 0],
+        [1, 1, 0, 1],
+        [1, 0, 1, 0]
+    ]
+
+    m = 3
+
+    g.graph_coloring(m)
